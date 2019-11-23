71bfb912da6f5af0e1ffac897e720be5fb7394e1
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-11-23 20:27:40 +0530

    release - v 1.1.1

--- build/fortjs-swagger.js
+++ build/fortjs-swagger.js
@@ -1,5 +1,5 @@
 /*!
- * @license :fortjs-swagger - V1.1.0 - 23/11/2019
+ * @license :fortjs-swagger - V1.1.1 - 23/11/2019
  * https://github.com/ujjwalguptaofficial/fortjs-swagger
  * Copyright (c) 2019 @Ujjwal Gupta; Licensed MIT
  */
